b537fac6db999759d334ff39ed37595989ac0e85
Marlon Lazzarotti <mlazzarotti@gmail.com>
2020-02-12 14:26:15 -0300

    add files via upload

---
+++ e32.py
@@ -0,0 +1,12 @@
+#32. Faça um programa que receba três números, calcule e mostre a multiplicação desses números.
+
+def e32():
+
+	n1 = int(input("Digite o primeiro número: "))
+	n2 = int(input("Digite o segundo número: ")) 
+	n3 = int(input("Digite o terceiro número: ")) 
+
+	print(f'O resultado de {n1} x {n2} x {n3} é {n1*n2*n3}')
+
+if(__name__ == "__main__"):
+    e32()
---
+++ lista.py
@@ -0,0 +1,83 @@
+#LISTA - CAPITULO 3 - EXERCICIOS PROPOSTOS
+
+# 1. Faça um programa que receba dois números, calcule e mostre a subtração do primeiro número pelo
+# segundo.
+# 2. Faça um programa que receba três números, calcule e mostre a multiplicação desses números.
+# 3. Faça um programa que receba dois números, calcule e mostre a divisão do primeiro número pelo
+# segundo. sabe-se que o segundo número não pode ser zero, portanto, não é necessário se preocupar
+# com validações.
+# 4. Faça um programa que receba duas notas, calcule e mostre a média ponderada dessas notas, considerando peso 2 para a primeira e peso 3 para a segunda.
+# 5. Faça um programa que receba o preço de um produto, calcule e mostre o novo preço, sabendo-se
+# que este sofreu um desconto de 10%.
+# 6. Um funcionário recebe um salário fixo mais 4% de comissão sobre as vendas. Faça um programa
+# que receba o salário fixo do funcionário e o valor de suas vendas, calcule e mostre a comissão e seu
+# salário final.
+# 7. Faça um programa que receba o peso de uma pessoa, calcule e mostre:
+# a) o novo peso, se a pessoa engordar 15% sobre o peso digitado;
+# b) o novo peso, se a pessoa emagrecer 20% sobre o peso digitado.
+# 8. Faça um programa que receba o peso de uma pessoa em quilos, calcule e mostre esse peso em gramas.
+# 9. Faça um programa que calcule e mostre a área de um trapézio.
+# sabe-se que: A = ((base maior + base menor) * altura)/2
+# 10. Faça um programa que calcule e mostre a área de um quadrado. sabe-se que: A = lado * lado.
+# 11. Faça um programa que calcule e mostre a área de um losango. sabe-se que: A = (diagonal maior * diagonal menor)/2.
+# 12. Faça um programa que receba o valor do salário mínimo e o valor do salário de um funcionário, calcule
+# e mostre a quantidade de salários mínimos que esse funcionário ganha.
+# 13. Faça um programa que calcule e mostre a tabuada de um número digitado pelo usuário.
+# Exemplo:
+# Digite um número: 5
+# 5 × 0 = 0
+# 5 × 1 = 5
+# 5 × 2 = 10
+# 5 × 3 = 15
+# 5 × 4 = 20
+# 5 × 5 = 25
+# 5 × 6 = 30
+# 5 × 7 = 35
+# 5 × 8 = 40
+# 5 × 9 = 45
+# 5 × 10 = 50
+# 14. Faça um programa que receba o ano de nascimento de uma pessoa e o ano atual, calcule e mostre:
+# a) a idade dessa pessoa em anos;
+# b) a idade dessa pessoa em meses;
+# c) a idade dessa pessoa em dias;
+# d) a idade dessa pessoa em semanas.
+# 15. João recebeu seu salário e precisa pagar duas contas atrasadas. Em razão do atraso, ele deverá pagar
+# multa de 2% sobre cada conta. Faça um programa que calcule e mostre quanto restará do salário de
+# João.
+# 16. Faça um programa que receba o valor dos catetos de um triângulo, calcule e mostre o valor da hipotenusa.
+# 17. Faça um programa que receba o raio, calcule e mostre:
+# a) o comprimento de uma esfera; sabe-se que c = 2 * p R;
+# b) a área de uma esfera; sabe-se que A = p R2;
+# c) o volume de uma esfera; sabe-se que v = ¾ * p R3.
+# 18. Faça um programa que receba uma temperatura em celsius, calcule e mostre essa temperatura em
+# Fahrenheit. sabe-se que F = 180*(c + 32)/100.
+# 19. sabe-se que, para iluminar de maneira correta os cômodos de uma casa, para cada m2, deve-se usar 18 W
+# de potência. Faça um programa que receba as duas dimensões de um cômodo (em metros), calcule e mostre
+# a sua área (em m2) e a potência de iluminação que deverá ser utilizada.
+# 20. Faça um programa que receba a medida do ângulo formado por uma escada apoiada no chão e a distância em que a escada está da parede, calcule e mostre a medida da escada para que se possa alcançar
+# sua ponta.
+# a
+# Medida da escada
+# Distância que a escada está da parede
+# 21. Faça um programa que receba o número de horas trabalhadas, o valor do salário mínimo e o número
+# de horas extras trabalhadas, calcule e mostre o salário a receber, de acordo com as regras a seguir:
+# a) a hora trabalhada vale 1/8 do salário mínimo;
+# b) a hora extra vale 1/4 do salário mínimo;52 | Fundamentos da programação de computadores
+# c) o salário bruto equivale ao número de horas trabalhadas multiplicado pelo valor da hora trabalhada;
+# d) a quantia a receber pelas horas extras equivale ao número de horas extras trabalhadas multiplicado pelo valor
+# da hora extra;
+# e) o salário a receber equivale ao salário bruto mais a quantia a receber pelas horas extras.
+# 22. Faça um programa que receba o número de lados de um polígono convexo, calcule e mostre o número
+# de diagonais desse polígono. sabe-se que ND = N * (N − 3)/2, em que N é o número de lados do
+# polígono.
+# 23. Faça um programa que receba a medida de dois ângulos de um triângulo, calcule e mostre a medida do
+# terceiro ângulo. sabe-se que a soma dos ângulos de um triângulo é 180 graus.
+# 24. Faça um programa que receba a quantidade de dinheiro em reais que uma pessoa que vai viajar possui. Ela
+# vai passar por vários países e precisa converter seu dinheiro em dólares, marco alemão e libra esterlina. sabe-
+# -se que a cotação do dólar é de R$ 1,80; do marco alemão, de R$ 2,00; e da libra esterlina, de R$ 3,57. O
+# programa deve fazer as conversões e mostrá-las.
+# 25. Faça um programa que receba uma hora (uma variável para hora e outra para minutos), calcule e
+# mostre:
+# a) a hora digitada convertida em minutos;
+# b) o total dos minutos, ou seja, os minutos digitados mais a conversão anterior;
+# c) o total dos minutos convertidos em segundos.
